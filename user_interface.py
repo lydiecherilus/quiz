fc565c73e3f18b3a8232d3b31c6021c4a01cb23a
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-16 22:59:41 -0400

    create UserInferface class

---
+++ user_interface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class UserInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz")
+        self.window.config(padx=30, pady=30, bg="blue")
+
+        self.window.mainloop()
+        
