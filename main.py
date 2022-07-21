0c8bad484fdae84c85457d73db64b98e529991d7
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-21 00:20:28 -0400

    make api call

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import requests
 from user_interface import UserInterface
 
 # set up user interface
 user_interface = UserInterface()
 
-# set up game
+# make api call
+parameters = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+question_answer = response.json()
