86847315b671146bacadc8e22466d96b4681f645
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-22 23:58:30 -0400

    create canvas

--- user_interface.py
+++ user_interface.py
@@ -7,5 +7,14 @@ class UserInterface:
         self.window.title("Quiz")
         self.window.config(padx=30, pady=30, bg="blue")
 
+        # create label
+        self.score = Label(text="Score: 0", fg="white", bg="blue")
+        self.score.grid(row=0, column=1)
+
+        # create canvas
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text((150, 125), width=280, text="", fill="blue", font=("Arial", 16))
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
         self.window.mainloop()
         
