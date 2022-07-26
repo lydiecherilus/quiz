e31b6e969d5f741f2c5f49f2756e99bc35525c99
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-25 23:50:55 -0400

    create buttons

--- user_interface.py
+++ user_interface.py
@@ -16,5 +16,14 @@ class UserInterface:
         self.question_text = self.canvas.create_text((150, 125), width=280, text="", fill="blue", font=("Arial", 16))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
+        # create buttons
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
+
         self.window.mainloop()
         
