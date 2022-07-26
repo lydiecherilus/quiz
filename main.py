d8e5e6b338732612b5be85b73350dc32e59524be
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-26 01:19:13 -0400

    set up questions

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 from user_interface import UserInterface
+from question_format import Question
 
-# set up user interface
-user_interface = UserInterface()
 
 # make api call
 parameters = {
@@ -12,4 +11,15 @@ parameters = {
 
 response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
-question_answer = response.json()
+questions_answers = response.json()
+
+# set up user interface
+user_interface = UserInterface()
+
+# set up questions
+all_questions = []
+for question in questions_answers["results"]:
+    quiz_question = question["question"]
+    quiz_answer = question["correct_answer"]
+    new_question = Question(quiz_question, quiz_answer)
+    all_questions.append(new_question)
