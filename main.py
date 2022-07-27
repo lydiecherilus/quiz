4895d145cdf93c2ac5c27fae60a7b45c9fc576a8
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-27 05:52:33 -0400

    setup quiz

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from user_interface import UserInterface
 from question_format import Question
+from quiz_format import Quiz
 
 
 # make api call
@@ -13,9 +14,6 @@ response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 questions_answers = response.json()
 
-# set up user interface
-user_interface = UserInterface()
-
 # set up questions
 all_questions = []
 for question in questions_answers["results"]:
@@ -23,3 +21,9 @@ for question in questions_answers["results"]:
     quiz_answer = question["correct_answer"]
     new_question = Question(quiz_question, quiz_answer)
     all_questions.append(new_question)
+
+# set quiz
+quiz_format = Quiz(all_questions)
+
+# set up user interface
+user_interface = UserInterface()
