5188744e7cd5d320b751f50ebf7b344c8013d986
Lydie Cherilus <lydiecherilus@yahoo.fr>
2022-07-29 10:41:40 -0400

    create function to check answer

--- quiz_format.py
+++ quiz_format.py
@@ -13,3 +13,13 @@ class Quiz:
         self.question_number += 1
         question_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {question_text}"
+
+    def check_answer(self, user_answer):
+        '''check user's answers againt quiz and update score'''
+        correct_answer = self.current_question.answer
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            return True
+        else:
+            return False
+            
